18df6902f0b759d4168612c5a932b8331b6ec8e3
Otim Isaac <isaacomega16@gmail.com>
2024-12-21 00:30:32 +0300

    position content to start of page

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -10,7 +10,7 @@ export default function Edit() {
     ? JSON.parse(decodeURIComponent(taskDataParam))
     : null;
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 sm:px-8 py-8 pb-20 gap-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-start min-h-screen px-4 sm:px-8 py-4 pb-20 gap-8 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full max-w-md sm:max-w-lg lg:max-w-xl flex flex-col gap-4 sm:gap-8 items-center sm:items-start">
         <Task task={taskData} />
       </main>
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -2,7 +2,7 @@ import Task from "../components/Task";
 
 export default function Create() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 sm:px-8 py-8 pb-20 gap-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="flex flex-col items-center justify-start min-h-screen px-4 sm:px-8 py-4 pb-20 gap-8 font-[family-name:var(--font-geist-sans)]">
       <main className="w-full max-w-md sm:max-w-lg lg:max-w-xl flex flex-col gap-4 sm:gap-8 items-center sm:items-start">
         <Task />
       </main>

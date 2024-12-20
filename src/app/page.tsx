ccc21a3f6c1eca709abf6e2565fca5701fc33930
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 22:28:42 +0300

    position create task button halfway on top of the logo bottom border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import Tasks from "./components/Tasks";
 export default function Home() {
   return (
     <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="w-full max-w-[736px] flex flex-col gap-8 items-start">
+      <main className="w-full max-w-[736px] flex flex-col gap-8 items-start relative">
         <Link
           href="/create"
-          className="inline-flex items-center justify-center gap-2 rounded-md px-4 py-2 text-sm text-white focus:relative bg-[#1E6F9F] w-full"
+          className="absolute -top-24 left-0 right-0 inline-flex items-center justify-center gap-2 rounded-md px-4 py-2 text-sm text-white focus:relative bg-[#1E6F9F] w-full z-10"
         >
           Create Task
           <svg
@@ -20,11 +20,8 @@ export default function Home() {
             strokeWidth="2"
             strokeLinecap="round"
             strokeLinejoin="round"
-            className="lucide lucide-circle-plus"
           >
-            <circle cx="12" cy="12" r="10" />
-            <path d="M8 12h8" />
-            <path d="M12 8v8" />
+            <path d="M12 5v14M5 12h14" />
           </svg>
         </Link>
         <Tasks />

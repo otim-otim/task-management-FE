c2a1346c6e445d08dbe60dd150bcf77b2b087a3a
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 21:35:48 +0300

    position content in edit task view

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -8,8 +8,8 @@ export default function Edit() {
 const taskDataParam = searchParams.get('taskData');
 const taskData = taskDataParam ? JSON.parse(decodeURIComponent(taskDataParam)) : null;
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+        <div className="flex flex-col items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="w-full max-w-[736px] flex flex-col gap-8 items-start">
     <Task task={taskData} />
         </main>
         </div>

6720d5fbd1b0bf23926c0ec009a38229a2b604df
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 22:13:20 +0300

    implement the back button

---
+++ src/app/components/BackButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+export default function BackButton() {
+  const router = useRouter();
+
+  const goBack = () => {
+    router.back();
+  };
+
+  return (
+    <button 
+      onClick={goBack}
+      className="flex items-center gap-2 text-gray-500 hover:text-gray-700"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-arrow-left"
+      >
+        <path d="m12 19-7-7 7-7" />
+        <path d="M19 12H5" />
+      </svg>
+      
+    </button>
+  );
+}

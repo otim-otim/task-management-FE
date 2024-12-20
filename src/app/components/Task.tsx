0ebcc15700aed9e217d2da06e6e3dc88e60674e0
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 19:18:46 +0300

    create or edit task

---
+++ src/app/components/Task.tsx
@@ -0,0 +1,119 @@
+"use client";
+
+import { createTask, updateTask } from "@/services/tasks.service";
+import { ITask, ITaskCreateResquest, ITaskUpdateRequest, TaskColor } from "@/types";
+import { useState } from "react";
+import { useRouter } from 'next/navigation'
+
+export default function Task({ task }: { task?: ITask }) {
+  const [title, setTitle] = useState<string | undefined>(task?.title);
+  const [selectedColor, setSelectedColor] = useState<TaskColor>("gray");
+  const router = useRouter()
+  const COLOR_CLASSES: Record<TaskColor, string> = {
+    red: "bg-red-500 hover:bg-red-600",
+    blue: "bg-blue-500 hover:bg-blue-600",
+    green: "bg-green-500 hover:bg-green-600",
+    yellow: "bg-yellow-500 hover:bg-yellow-600",
+    purple: "bg-purple-500 hover:bg-purple-600",
+    orange: "bg-orange-500 hover:bg-orange-600",
+    gray: "bg-gray-500 hover:bg-gray-600",
+  };
+
+  async function handleTaskCreateUpdate() {
+    if(title){
+        if(task) {
+          const taskUpdateRequest: ITaskUpdateRequest = {
+            title,
+            color: selectedColor,
+          };
+          await updateTask(taskUpdateRequest, task.id);
+        }else{
+            const taskCreateRequest: ITaskCreateResquest = {
+              title,
+              color: selectedColor,
+            };
+            await createTask(taskCreateRequest);
+    
+        }
+        router.push('/')
+
+    }
+}
+  const renderColorRadioGroup = () => {
+    return (
+      <div className="flex space-x-2">
+        {(Object.keys(COLOR_CLASSES) as TaskColor[]).map((color) => (
+          <label
+            key={color}
+            className={`
+                  inline-block w-8 h-8 rounded-full cursor-pointer 
+                  ${COLOR_CLASSES[color]}
+                  ${
+                    selectedColor === color
+                      ? "ring-4 ring-offset-2 ring-blue-300"
+                      : ""
+                  }
+                `}
+          >
+            <input
+              type="radio"
+              name="taskColor"
+              value={color}
+              checked={selectedColor === color}
+              onChange={() => setSelectedColor(color)}
+              className="hidden"
+            />
+          </label>
+        ))}
+      </div>
+    );
+  };
+  return (
+    //     <div className="flex flex-col gap-8 row-start-2 items-center sm:items-start w-93">
+    //   <div className=" items-center cursor-pointer items-start gap-4 rounded-lg border border-gray-200 p-4 transition hover:bg-gray-50 space-y-2">
+
+    <div className="max-w-md mx-auto py-6 bg-white ">
+      <label className="block text-sm font-medium text-blue-500">Title</label>
+      <div className="flex items-center mb-2">
+        &#8203;
+        <input
+          type="text"
+          className=" bg-gray-50 text-gray-900 text-sm  focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          value={title}
+          placeholder="e.g finish project"
+          onChange={(e) => setTitle(e.target.value)}
+        />
+      </div>
+      <label className="block text-sm font-medium text-blue-500">Color</label>
+      {renderColorRadioGroup()}
+      <button
+        onClick={handleTaskCreateUpdate}
+        className="inline-flex items-center justify-center gap-2 rounded-md px-4 py-2 text-sm text-white focus:relative bg-blue-500 w-full mt-4"
+      >
+        { task ?'save' : 'Add Task' }
+        {!task && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-circle-plus"
+        >
+          <circle cx="12" cy="12" r="10" />
+          <path d="M8 12h8" />
+          <path d="M12 8v8" />
+        </svg>)}
+      </button>
+      {/* <div className="mt-4">
+        <p>Selected Color: <span className="font-bold">{selectedColor}</span></p>
+      </div> */}
+    </div>
+    // </div>
+    // </div>
+  );
+}

4a23d8fb7bfd59417161fb30a71ad5938771fa43
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 20:25:21 +0300

    clean up task item component structure

--- src/app/components/TaskItem.tsx
+++ src/app/components/TaskItem.tsx
@@ -2,7 +2,7 @@
 
 import { ITask, ITaskUpdateRequest } from "@/types";
 import { useRouter } from "next/navigation";
-import { MouseEvent } from "react";
+import { ChangeEvent, MouseEvent } from "react";
 
 export default function TaskItem({
   task,
@@ -18,22 +18,21 @@ export default function TaskItem({
 }) {
   const router = useRouter();
   function printTasktitle() {
-    if (task.title.length > 20) {
-      return task.title.substring(0, 20) + "...";
+    if (task.title.length > 100) {
+      return task.title.substring(0, 100) + "...";
     } else {
       return task.title;
     }
   }
 
-  function handleTaskCompletion(e: MouseEvent) {
+  function handleTaskCompletion(e: ChangeEvent<HTMLInputElement>) {
     e.preventDefault();
-    e.stopPropagation();
-    const isCompleted = (e.target as HTMLInputElement).checked;
+    const isCompleted = e.target.checked;
     handleTaskEdit({
       taskId: task.id,
       updateReq: { completed: isCompleted },
     });
-    console.log("Task completed", (e.target as HTMLInputElement).checked);
+    console.log("Task completed", e.target.checked);
   }
 
   function onDeleteTask(e: MouseEvent) {
@@ -41,29 +40,31 @@ export default function TaskItem({
     e.stopPropagation();
     handleDeleteTask(task.id);
   }
-  function handleTaskNavigation(e: MouseEvent) {
+  function handleTaskNavigation(e: MouseEvent<HTMLDivElement>) {
     // Only navigate if the click is not on the checkbox or delete button
     const target = e.target as HTMLElement;
     if (
       !target.closest('input[type="checkbox"]') &&
       !target.closest("button")
     ) {
-      router.push(
-        `/${task.id}?taskData=${encodeURIComponent(JSON.stringify(task))}`
-      );
+      if (task.id) {
+        router.push(
+          `/${task.id}?taskData=${encodeURIComponent(JSON.stringify(task))}`
+        );
+      }
     }
   }
   return (
-    <div onClick={handleTaskNavigation} className="cursor-pointer">
-      <label
-        //   for="Option1"
-        className="flex cursor-pointer items-start gap-4 rounded-lg border border-gray-200 p-4 transition hover:bg-gray-50 has-[:checked]:bg-blue-50"
-      >
+    <div
+      onClick={handleTaskNavigation}
+      className="cursor-pointer w-[736px] h-[72px] pt-4 space-x-3 rounded-tl-lg border-t bg-[#262626] border border-[#333333] shadow-[0_2px_8px_0_#0000000F] flex items-center justify-between px-4"
+    >
+      <label className="flex cursor-pointer items-center gap-4 p-4">
         <div className="flex items-center">
           &#8203;
           <input
             type="checkbox"
-            className="size-4 rounded border-gray-300"
+            className="size-4 rounded-full border-2 border-blue-500 focus:ring-2 focus:ring-blue-300 checked:bg-blue-500"
             id="Option1"
             checked={task.completed}
             onChange={handleTaskCompletion}
@@ -71,38 +72,34 @@ export default function TaskItem({
         </div>
 
         <div>
-          <strong className="font-medium text-gray-900">
-            {" "}
-            {printTasktitle()}{" "}
+          <strong className="font-medium text-sm text-white-900">
+            {printTasktitle()}
           </strong>
-
-          {/* <p className="mt-1 text-pretty text-sm text-gray-700">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p> */}
-        </div>
-        <div>
-          <button onClick={onDeleteTask}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="lucide lucide-trash-2"
-            >
-              <path d="M3 6h18" />
-              <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-              <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-              <line x1="10" x2="10" y1="11" y2="17" />
-              <line x1="14" x2="14" y1="11" y2="17" />
-            </svg>
-          </button>
         </div>
       </label>
+
+      <div>
+        <button onClick={onDeleteTask}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="lucide lucide-trash-2"
+          >
+            <path d="M3 6h18" />
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+            <line x1="10" x2="10" y1="11" y2="17" />
+            <line x1="14" x2="14" y1="11" y2="17" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }

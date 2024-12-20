f3d55a600f02642131ce7925b1ca2d289ca56d4d
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 20:37:02 +0300

    clean up logo section element structure

--- src/app/components/Logo.tsx
+++ src/app/components/Logo.tsx
@@ -1,21 +1,40 @@
-import Image from 'next/image'
-import logo from '@/assets/images/logo.png'
+import Image from "next/image";
+import logo from "@/assets/images/logo.png";
 
 export default function Logo() {
-    const layout = {
-        width: '226px',
-        height: '48px',
-        top: '72px',
-        left: '657px',
-        gap: '0px',
-        opacity: '0px',
+  // const layout = {
+  //     width: '226px',
+  //     height: '48px',
+  //     top: '72px',
+  //     left: '657px',
+  //     gap: '0px',
+  //     opacity: '0px',
 
-    }
-    return (
-
-        <div className="flex items-center gap-2" style={layout}>
-            <Image className="w-10" width={100} height={100} src={logo} alt="Logo" /> 
-            {/* <span className="text-2xl font-bold">Pokédex</span> */}
-        </div>
-    )
+  // }
+  const layout = {
+    width: "1440px",
+    height: "200px",
+    top: "72px",
+    left: "657px",
+    gap: "0px",
+    opacity: "0px",
+    background: "#0D0D0D",
+  };
+  const imageStyle = {
+    width: "226px",
+    height: "48px",
+    top: "72px",
+    left: "657px",
+    gap: "0px",
+    opacity: "0px",
+  };
+  return (
+    <div
+      className="flex items-center justify-center w-full h-full"
+      style={layout}
+    >
+      <Image src={logo} alt="Nooro Logo" style={imageStyle} />
+      {/* <span className="text-2xl font-bold">Pokédex</span> */}
+    </div>
+  );
 }

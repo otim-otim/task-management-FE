3f9a0225173ba8102a6b16666c4c875413c349b8
Otim Isaac <isaacomega16@gmail.com>
2024-12-20 22:31:16 +0300

    use white color for backbutton icon

--- src/app/components/BackButton.tsx
+++ src/app/components/BackButton.tsx
@@ -12,7 +12,7 @@ export default function BackButton({ className, ...props }: React.HTMLAttributes
   return (
     <button 
       onClick={goBack}
-      className={`flex items-center gap-2 text-gray-500 hover:text-gray-700 ${className}`}
+      className={`flex items-center gap-2 text-white  ${className}`}
       {...props}
     >
       <svg
